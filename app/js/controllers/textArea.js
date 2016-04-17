063bef5ecdcde2eeb434561b799b97afa3697abc
Lunem <lydia.detienne@gmail.com>
2016-04-17 19:52:11 +0200

    exo1

---
+++ app/js/controllers/textArea.js
@@ -0,0 +1,11 @@
+textArea.controller("form",function(){
+  this.
+  // var self = this;
+  // self.count = 0;
+  // self.increment = function(){
+  //   self.count++;
+  // };
+  // self.decrement = function() {
+  //   self.count--;
+  // };
+});
